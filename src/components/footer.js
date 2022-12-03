b682a61df47eb1ffc276a4c17ab43652cae418aa
Baran Asan <baran.a.asan@outlook.com>
2022-12-03 15:24:31 +0100

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return(
+        <div className="footer">
+            <span>Made with</span>
+            <span>❤️</span>
+            <span>by</span>
+            <a href="" className="link">Baran</a>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <div className="header">
+            <a href="" id="logo">ResourceBack</a>
+        </div>
+    );
+};
+
+export default Header;
